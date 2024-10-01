0b98be98f1e0f64f97b8056dffc4659e3aa47566
icorani <icorani@yandex.ru>
2024-10-02 01:43:43 +0300

    feat: add func create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from "node:fs";
+import path from "node:path";
+
 const create = async () => {
-    // Write your code here 
+    const filename = 'fresh.txt';
+    const content = 'I am fresh and young';
+    const pathfile = path.join('files/fresh.txt');
+    console.log(pathfile)
+    fs.writeFile(pathfile, content, err => {
+        if (err) {
+            console.error('FS operation failed', err)
+        } else {
+            console.log('FS is OK')
+        }
+    });
 };
 
 await create();
