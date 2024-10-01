7b085045cdd82b16c414d4b75676630f6d76effd
icorani <icorani@yandex.ru>
2024-10-02 02:58:22 +0300

    feat: add copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,16 @@
+import path from "node:path";
+import fs from "node:fs";
+
 const copy = async () => {
-    // Write your code here 
+    const fromPath = path.dirname('files/.');
+    const goalPath = path.dirname('files_copy/.');
+    fs.cp(fromPath, goalPath,
+        {recursive: true},
+            err => {
+        if (err) {
+            console.error("FS operation failed", err)
+        }
+    });
 };
 
 await copy();
