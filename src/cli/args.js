2e7eea0273f3f34287353f0c9dee5d80769c222c
icorani <icorani@yandex.ru>
2024-10-02 22:37:58 +0300

    fix: update args.js task

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,9 +2,20 @@ import {argv} from 'node:process';
 
 const parseArgs = () => {
     const argsList = argv;
-    argv.forEach((val, index) => {
-        console.log(`${index}: ${val}`);
+    let resultString = '';
+    let stats = 2;
+    console.log(argv)
+    argv.slice(2,).forEach((key, value) => {
+        key = key.slice(2,);
+        if (stats === 2) {
+            resultString += `${key} is `;
+            stats = 1;
+        } else {
+            resultString += `${value}, `;
+            stats = 2;
+        }
     });
+    console.log(resultString.slice(0,-2))
 };
 
 parseArgs();
