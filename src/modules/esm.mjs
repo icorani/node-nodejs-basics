80f232b6544db9332baba7f2c1a5ce5476d773c6
icorani <icorani@yandex.ru>
2024-10-02 23:47:02 +0300

    feat: finish esm.mjs task

---
+++ src/modules/esm.mjs
@@ -0,0 +1,50 @@
+import path from 'node:path';
+import {release, version} from 'node:os';
+import http from 'node:http'
+import './files/c.js';
+
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import * as assert from "node:assert";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+
+const random = Math.random();
+
+let unknownObject;
+
+if (random > 0.5) {
+    unknownObject = require('./files/a.json');
+} else {
+    unknownObject = require('./files/b.json');
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = http.createServer((_, res) => {
+    res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
+
+export {
+    unknownObject,
+    myServer,
+};
+
