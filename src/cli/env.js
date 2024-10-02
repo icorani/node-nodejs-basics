4083c17b4263b53cc24acaba9cefb9b958bfeb09
icorani <icorani@yandex.ru>
2024-10-02 04:58:57 +0300

    feat: create print envs function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
+// to test this func
+// import { env } from 'node:process';
+// env.RSS_main_question = '42'
+// env.RSS_usual_deadline = '7 days'
+
+
 const parseEnv = () => {
-    // Write your code here 
+    const envList = process.env;
+    for (const s in envList) {
+        if (s.toString().startsWith('RSS_')) {
+            console.log(s, envList[s])
+        }
+    }
 };
 
 parseEnv();
