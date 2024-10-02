87f62714bc74cf9b659e6acf6c66994b7cd20700
icorani <icorani@yandex.ru>
2024-10-02 04:34:51 +0300

    feat: create read async function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,12 @@
+import {readFile} from 'node:fs/promises'
+
 const read = async () => {
-    // Write your code here 
+    try{
+        const fileContent = await readFile('files/fileToRead.txt', {encoding: 'utf8'});
+        console.log(fileContent);
+    } catch (err) {
+        console.log('FS operation failed');
+    }
 };
 
 await read();
