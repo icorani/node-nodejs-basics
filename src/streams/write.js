c139f1b65eb694626395e0d98bbb3871daba35eb
icorani <icorani@yandex.ru>
2024-10-04 00:48:57 +0300

    feat: finish write.js task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import {stdin, stdout} from 'node:process'
+import {writeFile, createWriteStream} from 'node:fs'
+
 const write = async () => {
-    // Write your code here 
+    const file = createWriteStream('files/fileToWrite.txt')
+    stdin.on('data', data => {
+        file.write(data);
+    })
+
 };
 
 await write();
