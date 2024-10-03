bce81b8e7cb2c7a6e94106a6fbb7fb5ee7af6aa4
icorani <icorani@yandex.ru>
2024-10-03 23:58:18 +0300

    feat: finish read.js task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
+import {createReadStream} from "node:fs";
+
 const read = async () => {
-    // Write your code here 
+    const stream = createReadStream('files/fileToRead.txt');
+    stream.on('data', function (chunk) {
+    console.log(chunk.toString());
+});
+
 };
 
 await read();
