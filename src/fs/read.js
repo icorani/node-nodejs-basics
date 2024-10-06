342b3010b181f9f1f3f254a191cce65d1ae4d20b
icorani <icorani@yandex.ru>
2024-10-06 23:51:13 +0300

    fix: refactor read.js task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,8 +1,10 @@
-import {readFile} from 'node:fs/promises'
+import {readFile} from 'node:fs/promises';
+import path from "node:path";
 
 const read = async () => {
     try{
-        const fileContent = await readFile('files/fileToRead.txt', {encoding: 'utf8'});
+        const fileContent = await readFile(path.resolve('src/fs/files/fileToRead.txt'),
+            {encoding: 'utf8'});
         console.log(fileContent);
     } catch (err) {
         throw Error('FS operation failed');
