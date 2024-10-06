5b40bf01deb8adbf32fd0ccba59aac8ecfeb068c
icorani <icorani@yandex.ru>
2024-10-06 05:36:41 +0300

    fix: refactor list.js task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,8 +1,9 @@
 import {readdir} from "node:fs/promises"
+import path from "node:path";
 
 const list = async () => {
     try {
-        const fileList = await readdir('files/');
+        const fileList = await readdir(path.resolve('src/fs/files/'));
         for (const file of fileList){
             console.log(file)
         }
