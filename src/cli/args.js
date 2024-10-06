ac3700e04471441ca77156ea0b9700508200d590
icorani <icorani@yandex.ru>
2024-10-07 00:37:07 +0300

    fix: refactor args.js task

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,19 +2,13 @@ import {argv} from 'node:process';
 
 const parseArgs = () => {
     const argsList = argv;
-    let resultString = '';
-    let stats = 2;
-    argv.slice(2,).forEach((key, value) => {
-        key = key.slice(2,);
-        if (stats === 2) {
-            resultString += `${key} is `;
-            stats = 1;
-        } else {
-            resultString += `${value}, `;
-            stats = 2;
+    let resultArray = [];
+    argv.slice(2,).forEach((key, index,value) => {
+        if (key.startsWith('--')){
+            resultArray.push(`${key.replace('--','')} is ${value[index+1]}`);
         }
     });
-    console.log(resultString.slice(0,-2))
+    console.log(resultArray.join(', '))
 };
 
 parseArgs();
