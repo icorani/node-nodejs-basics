8972dec26d97dc9a4a1bf7f2f04978f50d64e931
icorani <icorani@yandex.ru>
2024-10-06 05:32:58 +0300

    fix: refactor delete.js task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,7 +1,11 @@
-import {unlink} from "node:fs/promises"
+import {rm} from "node:fs/promises"
+import path from "node:path";
+
+const filePath = path.resolve('src/fs/files/fileToRemove.txt');
+
 const remove = async () => {
     try {
-        await unlink('files/fileToRemove.txt');
+        await rm(filePath);
     } catch (err) {
         console.error('FS operation failed')
     }
