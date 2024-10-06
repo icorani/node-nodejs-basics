93ca250279dfa66fc13dd2072e3ca87d1af6e46c
icorani <icorani@yandex.ru>
2024-10-07 00:40:10 +0300

    fix: refactor env.js task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,18 +1,13 @@
-// to test this func
-// import { env } from 'node:process';
-// env.RSS_main_question = '42'
-// env.RSS_usual_deadline = '7 days'
-
+const envList = process.env;
 
 const parseEnv = () => {
-    const envList = process.env;
-    let resultString = '';
+    let resultArray = [];
     Object.entries(envList).forEach(([key, value]) => {
         if (key.startsWith('RSS_')) {
-            resultString += `${key}=${value}; `;
+            resultArray.push(`${key}=${value}`);
         }
     });
-    console.log(resultString.slice(0,-2))
+    console.log(resultArray.join(', '))
 
 };
 
