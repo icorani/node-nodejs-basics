3dac990a0ce6edcd49b9fdbbda269fa791634c60
icorani <icorani@yandex.ru>
2024-10-07 02:28:20 +0300

    fix: update write.js task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,10 +1,11 @@
-import {stdin, stdout} from 'node:process'
-import {writeFile, createWriteStream} from 'node:fs'
+import {stdin} from 'node:process'
+import {createWriteStream} from 'node:fs'
 
 const write = async () => {
-    const file = createWriteStream('files/fileToWrite.txt')
-    stdin.on('data', data => {
-        file.write(data);
+    const fileWriteStream =
+        createWriteStream('src/streams/files/fileToWrite.txt', {flags: 'a'})
+    await stdin.on('data', data => {
+        fileWriteStream.write(data);
     })
 
 };
