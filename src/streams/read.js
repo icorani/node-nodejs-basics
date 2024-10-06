d53cdacf5bdf16f6e1ee890955a21101e471fb7f
icorani <icorani@yandex.ru>
2024-10-07 02:16:19 +0300

    fix: update read.js task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,7 +1,8 @@
 import {createReadStream} from "node:fs";
+import path from "node:path";
 
 const read = async () => {
-    const stream = createReadStream('files/fileToRead.txt');
+    const stream = createReadStream(path.resolve('src/streams/files/fileToRead.txt'));
     stream.on('data', (chunk) => {
         console.log(chunk.toString());
     });
