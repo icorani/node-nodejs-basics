8a183bbd55fb579e14bf744cf70d0d52ae2e26ab
icorani <icorani@yandex.ru>
2024-10-06 05:22:40 +0300

    fix: refactor to async func create.js task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,16 +1,24 @@
 import path from "node:path";
-import { copyFile, constants } from 'node:fs/promises';
+import {existsSync} from 'node:fs';
+import {cp, constants, access} from 'node:fs/promises';
 
-const fromPath = path.resolve('src/fs/files/.');
-const goalPath = path.resolve('src/fs/files_copy/.');
+
+const sourcePath = path.resolve('src/fs/files/.');
+const destPath = path.resolve('src/fs/files_copy/.');
 
 const copy = async () => {
+    if ((existsSync(destPath)) || (!existsSync(sourcePath))) {
+        throw Error('FS operation failed')}
     try {
-        await copyFile(fromPath, goalPath,constants.COPYFILE_EXCL)
-    } catch (err) {
-        console.log(err)
-        // throw Error('FS operation failed');
+            await cp(sourcePath, destPath,
+                {
+                    'errorOnExist': true,
+                    'recursive': true,
+                })
+        } catch (err) {
+            console.log(err)
+            // throw Error('FS operation failed');
+        }
     }
-};
 
 await copy();
