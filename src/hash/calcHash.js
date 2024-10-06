7cb0f957d5effcc462142b060a3c6acf9ee94e78
icorani <icorani@yandex.ru>
2024-10-07 02:13:09 +0300

    fix: refactor calcHash.js task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,18 +1,13 @@
-import {createReadStream, } from 'node:fs';
+import {readFile, } from 'fs/promises';
 
 const {createHash, } = await import ('node:crypto');
 const filename = 'src/hash/files/fileToCalculateHashFor.txt'
 const hash = createHash('sha256')
 
 const calculateHash = async () => {
-    const readStream = createReadStream(filename);
-    readStream.on('readable', ()=>{
-        const data = readStream.read();
-        if (data)
-            hash.update(data);
-        else
-            console.log(`${hash.digest('hex')}`)
-    })
+    const content = await readFile(filename);
+    hash.update(content);
+    console.log(`${hash.digest('hex')}`)
 };
 
 await calculateHash();
