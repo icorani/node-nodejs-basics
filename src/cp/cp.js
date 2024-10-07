6e77db5c8ce14f724efa2e2b44ff5b21d12c8444
icorani <icorani@ya.ru>
2024-10-07 23:35:37 +0300

    feat: finish childProcess task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import {fork} from 'node:child_process';
+import path from "node:path";
+import process from 'node:process';
+
+const childScriptPath = path.resolve('src/cp/files/script.js')
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess =
+        fork(childScriptPath, [args], {stdio: 'inherit'});
+    //'inherit option in stdio option is mean pass tru the corresponding parent process i/o stream
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess();
