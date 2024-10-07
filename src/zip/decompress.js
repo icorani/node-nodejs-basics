b7cf09cfa7a45924b58cb4ea0d78104a9d42ec90
icorani <icorani@yandex.ru>
2024-10-07 04:39:00 +0300

    feat: finish decompress.js task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,12 @@
+import {pipeline} from 'stream/promises';
+import {createReadStream, createWriteStream} from "fs";
+import {createGunzip} from 'node:zlib';
+import path from "node:path";
+
+const writeStream = createWriteStream(path.resolve('src/zip/files/fileToCompress.txt'));
+const readStream = createReadStream(path.resolve('src/zip/files/archive.gz'));
 const decompress = async () => {
-    // Write your code here 
+    await pipeline(readStream, createGunzip(), writeStream);
 };
 
 await decompress();
